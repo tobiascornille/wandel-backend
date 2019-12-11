48e65cc6a0fe821b78d681e0f3297c22f62b3949
Tobias Cornille <cornilletobias@gmail.com>
2019-12-11 23:00:38 +0000

    Add dynamic port for Heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000
+
 app.use(bodyParser.json())
 require('./routes/poi.routes.js')(app)
 
-const server = app.listen(8081, function () {
+const server = app.listen(PORT, function () {
 
     var host = server.address().address;
     var port = server.address().port;
