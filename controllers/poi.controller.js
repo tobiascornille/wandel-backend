caad2f9c569d9e358a54795f00c3709f4763e580
Bidaux <loic.bidaux@gmail.com>
2019-12-13 11:52:59 +0100

    Correct path 3 overpoppulated bug

--- controllers/poi.controller.js
+++ controllers/poi.controller.js
@@ -1,5 +1,5 @@
 const gMaps = require("../external/google.maps");
-const axios = require('axios');
+const axios = require("axios");
 var fs = require("fs");
 
 //This module implements all logic of choosing which POI(s) to visit
@@ -9,37 +9,50 @@ const poi = JSON.parse(data);
 exports.getRoutes = function(req, res) {
   var json_request = req.body;
   var destCoordsUrl = gMaps.getCoordinates(json_request.destination);
-  axios.get(destCoordsUrl)
-      .then((response) => {
-        json_request.destination = response.data.candidates[0].geometry.location;
-        var stops = stopList(json_request);
-        var route1 = gMaps.getRoute(json_request.origin, json_request.destination, stops[0]);
-        var route2 = gMaps.getRoute(json_request.origin, json_request.destination, stops[1]);
-        var route3 = gMaps.getRoute(json_request.origin, json_request.destination, stops[2]);
+  axios
+    .get(destCoordsUrl)
+    .then(response => {
+      json_request.destination = response.data.candidates[0].geometry.location;
+      var stops = stopList(json_request);
+      var route1 = gMaps.getRoute(
+        json_request.origin,
+        json_request.destination,
+        stops[0]
+      );
+      var route2 = gMaps.getRoute(
+        json_request.origin,
+        json_request.destination,
+        stops[1]
+      );
+      var route3 = gMaps.getRoute(
+        json_request.origin,
+        json_request.destination,
+        stops[2]
+      );
 
-        var result = [
-          {
-            routeName: "Route 1",
-            route: route1,
-            spots: stops[0]
-          },
-          {
-            routeName: "Route 2",
-            route: route2,
-            spots: stops[1]
-          },
-          {
-            routeName: "Route 3",
-            route: route3,
-            spots: stops[2]
-          }
-        ];
-    
-        return res.send(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      var result = [
+        {
+          routeName: "Route 1",
+          route: route1,
+          spots: stops[0]
+        },
+        {
+          routeName: "Route 2",
+          route: route2,
+          spots: stops[1]
+        },
+        {
+          routeName: "Route 3",
+          route: route3,
+          spots: stops[2]
+        }
+      ];
+
+      return res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 // stopList receives a request of structure:
@@ -73,7 +86,7 @@ function stopList(req) {
     viewpoint: req.preferences.Viewpoints,
     museum: req.preferences.Museums,
     landmark: req.preferences.Landmarks,
-    church: false,
+    church: false
   };
   var originDestDistance = rawDistance(req.origin, req.destination);
   var rawdata = fs.readFileSync("poi.json");
@@ -441,10 +454,24 @@ function stopList(req) {
       }
     } else {
       for (var i = 0; i < outsidePoi.length; i++) {
-        if (selectNotExisting(outsidePoi, finalPath3) != -1) {
-          finalPath3.push(
-            outsidePoi[selectNotExisting(outsidePoi, finalPath3)]
-          );
+        if (finalPath3.length < 3) {
+          if (
+            selectByPreference(preferenceList, outsidePoi, finalPath3) != -1
+          ) {
+            finalPath3.push(
+              outsidePoi[
+                selectByPreference(preferenceList, outsidePoi, finalPath3)
+              ]
+            );
+          } else {
+            if (selectNotExisting(outsidePoi, finalPath3) != -1) {
+              finalPath3.push(
+                outsidePoi[selectNotExisting(outsidePoi, finalPath3)]
+              );
+            }
+          }
+        } else {
+          break;
         }
       }
     }
@@ -653,56 +680,57 @@ function populateDetail() {
 }
 
 exports.test = function() {
-  testRequest.destination = 'Cais do Sodre';
+  testRequest.destination = "Cais do Sodre";
   var destCoordsUrl = gMaps.getCoordinates(testRequest.destination);
-  axios.get(destCoordsUrl)
-      .then((response) => {
-        console.log(response.data.candidates[0].geometry.location);
-        testRequest.destination = response.data.candidates[0].geometry.location;
-        var stops = stopList(testRequest);
-        var route1 = gMaps.getRoute(stops[0]);
-        var route2 = gMaps.getRoute(stops[1]);
-        var route3 = gMaps.getRoute(stops[2]);
+  axios
+    .get(destCoordsUrl)
+    .then(response => {
+      console.log(response.data.candidates[0].geometry.location);
+      testRequest.destination = response.data.candidates[0].geometry.location;
+      var stops = stopList(testRequest);
+      var route1 = gMaps.getRoute(stops[0]);
+      var route2 = gMaps.getRoute(stops[1]);
+      var route3 = gMaps.getRoute(stops[2]);
 
-        var res = [
-          {
-            routeName: "Route 1",
-            route: route1,
-            spots: []
-          },
-          {
-            routeName: "Route 2",
-            route: route2,
-            spots: []
-          },
-          {
-            routeName: "Route 3",
-            route: route3,
-            spots: []
-          }
-        ];
-
-        var spotsContent = [];
-        for (let i = 1; i < stops[0].length - 1; i++) {
-          spotsContent.push(stops[0][i]);
+      var res = [
+        {
+          routeName: "Route 1",
+          route: route1,
+          spots: []
+        },
+        {
+          routeName: "Route 2",
+          route: route2,
+          spots: []
+        },
+        {
+          routeName: "Route 3",
+          route: route3,
+          spots: []
         }
-        res[0].spots = spotsContent;
+      ];
 
-        spotsContent = [];
-        for (let i = 1; i < stops[1].length - 1; i++) {
-          spotsContent.push(stops[1][i]);
-        }
-        res[1].spots = spotsContent;
+      var spotsContent = [];
+      for (let i = 1; i < stops[0].length - 1; i++) {
+        spotsContent.push(stops[0][i]);
+      }
+      res[0].spots = spotsContent;
 
-        spotsContent = [];
-        for (let i = 1; i < stops[2].length - 1; i++) {
-          spotsContent.push(stops[2][i]);
-        }
-        res[2].spots = spotsContent;
+      spotsContent = [];
+      for (let i = 1; i < stops[1].length - 1; i++) {
+        spotsContent.push(stops[1][i]);
+      }
+      res[1].spots = spotsContent;
 
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      spotsContent = [];
+      for (let i = 1; i < stops[2].length - 1; i++) {
+        spotsContent.push(stops[2][i]);
+      }
+      res[2].spots = spotsContent;
+
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
